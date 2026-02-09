5e71f985d5852c34c60c2557a89bbe2bcfa84816
rohit-1092 <rohitmaurya1092@gmail.com>
2026-02-09 12:55:57 +0530

    Update API URLs and email settings for production

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const cors = require('cors');
 const connectDB = require('./config/db');
 const Platform = require('./models/Platform');
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -11,7 +11,7 @@ const Login = ({ onLoginSuccess, onNavigateToRegister, onNavigateToForgot }) => 
     e.preventDefault();
     try {
       // Backend route /send-otp par request
-      await axios.post('http://localhost:5000/api/auth/send-otp', { email });
+     await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/send-otp`, { email });
       alert("OTP aapki email par bhej diya gaya hai!");
       setStep(2); // Isse UI change hoga aur OTP box dikhega
     } catch (err) {
